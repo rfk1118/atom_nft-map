1c3066f429d71da0f547a342693124dfc119c075
renfakai <rfk1118@gmail.com>
2023-10-29 23:35:17 +0800

    fix

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import time
 import multiprocessing
 import re
 
 import requests
 
 requestUrl = "https://ep.atomicals.xyz/proxy/blockchain.atomicals.get"
+queryNow = "https://ep.atomicals.xyz/proxy/blockchain.atomicals.list"
+queryNowBody = {"params": [48, -1, 0]}
 max_retry = 3
 
 
@@ -20,6 +23,7 @@ def worker_function(start, end):
     result = []
     for num in range(start, end):
         find_file_name = ""
+        print(f"请求高度为{num}")
         body = {"params": [str(num)]}
         retry = 0
         while True:
@@ -74,19 +78,51 @@ def handler(start, end, num_processes):
     merged_array = []
     for arr in results:
         merged_array.extend(arr)
-    unique_numbers = list(set(merged_array))
-    merged_array = sorted(unique_numbers)
-    with open('output.txt', 'a') as file:
+
+    result = []
+    with open('output.txt', 'r+') as file:
         for item in merged_array:
             file.write(str(item) + "\n")
-    with open("output.txt", "r") as file:
         queryArray = [int(row.strip()) for row in file]
         queryArray = list(set(queryArray))
         queryArray = sorted(queryArray)
+        my_dict = {index: value for index, value in enumerate(queryArray)}
         for i in range(len(queryArray) - 1):
             if int(queryArray[i]) + 1 != int(queryArray[i + 1]):
                 print(f"在索引 {i} 和 {i + 1} 处的元素不满足条件: {queryArray[i]} ～ {queryArray[i + 1]}")
+        for i in range(1000, 9999):
+            if i in my_dict:
+                print(f"{i} 存在于字典中，对应的值是 {my_dict[i]}")
+            else:
+                print(f"{i} 不存在于字典中")
+                result.append(i)
+    with open("can_mint.txt", "w") as file:
+        for e in result:
+            file.write(str(e) + "\n")
+
+
+def record_and_loop():
+    while True:
+        try:
+            with open("current.txt", 'r+') as file:
+                queryArray = [int(row.strip()) for row in file]
+                current = queryArray[len(queryArray) - 1]
+                response = requests.post(queryNow, json=queryNowBody)
+                if response.status_code == 200:
+                    atomical_count = response.json()['response']['global']['atomical_count']
+                    if atomical_count is not None:
+                        atomical_count = atomical_count - 1
+                        if atomical_count > current:
+                            handler(current, atomical_count, 8)
+                            file.write(str(atomical_count) + "\n")
+                        else:
+                            print(f"current:{current},atomical_count:{atomical_count}")
+                else:
+                    print(response.json())
+        except Exception as e:
+            print(f"出现异常，睡眠10秒{e}")
+            time.sleep(10)
 
 
 if __name__ == '__main__':
-    handler(64200, 64627, 16)
+    record_and_loop()
