a4a1a4541a7ff0cd096a2fb8cad25514336d5248
renfakai <rfk1118@gmail.com>
2023-11-09 10:33:07 +0800

    最新索引

--- main.py
+++ main.py
@@ -31,39 +31,35 @@ def worker_function(start, end):
                 if retry >= max_retry:
                     break
                 response = requests.post(requestUrl, json=body)
-                if response.status_code == 200:
-                    mint_data = response.json()['response']['result']['mint_data']['fields']
-                    bit_work_c = mint_data['args']['bitworkc']
-                    print(f"bit_work_c:{bit_work_c}~{mint_data['args']}")
-                    if mint_data['args'] is None:
-                        break
-                    if mint_data['args']['bitworkc'] is None:
-                        break
-                    for key in mint_data:
-                        if "atommap.svg" in key:
-                            print(f"{bit_work_c}～{key}")
-                            find_file_name = key
-                    if len(find_file_name) == 0:
-                        retry = retry + 1
-                        continue
-                    match = re.search(r'\b\d+\b', find_file_name[::-1])
-                    if match:
-                        last_number_reversed = match.group()[::-1]  # 反转回来以得到最后一个数字
-                        bit_work_c = bit_work_c.replace("ab", "")
-                        if last_number_reversed != bit_work_c:
-                            print("miss match" + bit_work_c + ":" + last_number_reversed)
-                            break
-                        else:
-                            print("push:" + last_number_reversed)
-                            result.append(int(last_number_reversed))
-                            break
-                    else:
-                        print(f"{find_file_name}未找到数字")
-                        retry = retry + 1
-                        continue
-                else:
-                    print(f"Request failed with status code {response.status_code}:{response.text}")
+                if response.status_code != 200:
+                    print(f"{find_file_name}未找到数字")
                     retry = retry + 1
+                    continue
+                mint_data = response.json()['response']['result']['mint_data']['fields']
+                bit_work_c = mint_data['args']['bitworkc']
+                print(f"bit_work_c:[{bit_work_c}:{mint_data['args']}]")
+                if mint_data['args'] is None:
+                    break
+                if mint_data['args']['bitworkc'] is None:
+                    break
+                for key in mint_data:
+                    if "atommap.svg" in key:
+                        print(f"{bit_work_c}～{key}")
+                        find_file_name = key
+                if len(find_file_name) == 0:
+                    retry = retry + 1
+                    continue
+                match = re.search(r'\b\d+\b', find_file_name[::-1])
+                if match:
+                    last_number_reversed = match.group()[::-1]  # 反转回来以得到最后一个数字
+                    bit_work_c = bit_work_c.replace("ab", "")
+                    if last_number_reversed != bit_work_c:
+                        print("miss match" + bit_work_c + ":" + last_number_reversed)
+                        break
+                    else:
+                        print("push:" + last_number_reversed)
+                        result.append(int(last_number_reversed))
+                        break
             except Exception as e:
                 retry = retry + 1
                 print(e)
@@ -78,7 +74,6 @@ def handler(start, end, num_processes):
     merged_array = []
     for arr in results:
         merged_array.extend(arr)
-
     result = []
     with open('output.txt', 'r+') as file:
         for item in merged_array:
@@ -90,7 +85,7 @@ def handler(start, end, num_processes):
         for i in range(len(queryArray) - 1):
             if int(queryArray[i]) + 1 != int(queryArray[i + 1]):
                 print(f"在索引 {i} 和 {i + 1} 处的元素不满足条件: {queryArray[i]} ～ {queryArray[i + 1]}")
-        for i in range(1000, 9999):
+        for i in range(10000, 99999):
             if i in my_dict:
                 print(f"{i} 存在于字典中，对应的值是 {my_dict[i]}")
             else:
@@ -108,40 +103,40 @@ def record_and_loop():
                 queryArray = [int(row.strip()) for row in file]
                 current = queryArray[len(queryArray) - 1]
                 response = requests.post(queryNow, json=queryNowBody)
-                if response.status_code == 200:
-                    atomical_count = response.json()['response']['global']['atomical_count']
-                    if atomical_count is not None:
-                        atomical_count = atomical_count - 1
-                        if atomical_count > current:
-                            handler(current, atomical_count, 2)
-                            file.write(str(atomical_count) + "\n")
-                        else:
-                            print(f"current:{current},atomical_count:{atomical_count}")
-                else:
+                if response.status_code != 200:
                     print(response.json())
+                    continue
+                atomical_count = response.json()['response']['global']['atomical_count']
+                if atomical_count is not None:
+                    atomical_count = atomical_count - 1
+                    if atomical_count > current:
+                        handler(current, atomical_count, 2)
+                        file.write(str(atomical_count) + "\n")
+                    else:
+                        print(f"current:{current},atomical_count:{atomical_count}")
         except Exception as e:
             print(f"出现异常，睡眠10秒{e}")
             time.sleep(10)
 
 
 if __name__ == '__main__':
-    # record_and_loop()
-    merged_array = []
-    result = []
-    with open('output.txt', 'r+') as file:
-        queryArray = [int(row.strip()) for row in file]
-        queryArray = list(set(queryArray))
-        queryArray = sorted(queryArray)
-        my_dict = {value: value for index, value in enumerate(queryArray)}
-        for i in range(len(queryArray) - 1):
-            if int(queryArray[i]) + 1 != int(queryArray[i + 1]):
-                print(f"在索引 {i} 和 {i + 1} 处的元素不满足条件: {queryArray[i]} ～ {queryArray[i + 1]}")
-        for i in range(1000, 9999):
-            if i in my_dict:
-                print(f"{i} 存在于字典中，对应的值是 {my_dict[i]}")
-            else:
-                print(f"{i} 不存在于字典中")
-                result.append(i)
-    with open("can_mint.txt", "w") as file:
-        for e in result:
-            file.write(str(e) + "\n")
+    record_and_loop()
+    # merged_array = []
+    # result = []
+    # with open('output.txt', 'r+') as file:
+    #     queryArray = [int(row.strip()) for row in file]
+    #     queryArray = list(set(queryArray))
+    #     queryArray = sorted(queryArray)
+    #     my_dict = {value: value for index, value in enumerate(queryArray)}
+    #     for i in range(len(queryArray) - 1):
+    #         if int(queryArray[i]) + 1 != int(queryArray[i + 1]):
+    #             print(f"在索引 {i} 和 {i + 1} 处的元素不满足条件: {queryArray[i]} ～ {queryArray[i + 1]}")
+    #     for i in range(1000, 9999):
+    #         if i in my_dict:
+    #             print(f"{i} 存在于字典中，对应的值是 {my_dict[i]}")
+    #         else:
+    #             print(f"{i} 不存在于字典中")
+    #             result.append(i)
+    # with open("can_mint.txt", "w") as file:
+    #     for e in result:
+    #         file.write(str(e) + "\n")
